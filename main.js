b03d499dd6f58477c44cc3fb7adf60b2cbd6fa83
Jeff Stapleton <jeffstapleton@ufl.edu>
2018-10-17 23:08:59 -0500

    Move var yourFortune and fortuneTextPlaceholder to top

--- main.js
+++ main.js
@@ -1,6 +1,9 @@
 var magicFortunes = ['It is certain.','It is decidedly so.', 'Without a doubt.', 'Yes - definitely.', 'You may rely on it.', 'As I see it, yes.', 'Most likely.', 'Outlook good.', 'Yes.', 'Signs point to yes.', 'Reply hazy, try again', 'Ask again later.', 'Better not tell you now.', 'Cannot predict now.', 'Concentrate and ask again.', 'Dont count on it.', 'My reply is no.', 'My sources say no.', 'Outlook not so good.', 'Very doubtful.'];
 
 var button = document.getElementsByTagName('button')[0];
+var yourFortune = document.querySelector('#fortune-wrapper > p > span');
+var fortuneTextPlaceholder = document.createTextNode('');
+yourFortune.appendChild(fortuneTextPlaceholder);
 
 function shakeBall() {
     magicFortunes.sort(function(a, b){
@@ -16,6 +19,3 @@ function shakeBall() {
 
 button.addEventListener('click', shakeBall);
 
-var yourFortune = document.querySelector('#fortune-wrapper > p > span');
-var fortuneTextPlaceholder = document.createTextNode('');
-yourFortune.appendChild(fortuneTextPlaceholder);
